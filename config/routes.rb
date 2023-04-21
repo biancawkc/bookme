c48c9633af7be610a73d859a633f5968936de916
Bianca Chu <biancawkc@gmail.com>
2023-04-20 23:09:21 -0300

    Update: Booking link + Booking types

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :booking_types
-  resources :bookings
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
@@ -14,6 +12,11 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: "home#dashboard", as: :authenticated_root
   end
+
+  resources :booking_types
+  resources :bookings
+
+  get ":booking_link", to: "users#show", as: :user
 
   root :to => redirect("/users/sign_in")
 end
