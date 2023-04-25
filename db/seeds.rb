60269d0a134ff8579f7459d090018c44f02260ea
Bianca Chu <biancawkc@gmail.com>
2023-04-25 13:38:39 -0300

    Add: initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+BookingType.destroy_all
+User.destroy_all
+
+user = User.create!(
+    booking_link: "jmisth",
+    email: "john.smith@gmail.com",
+    name: "John Smith",
+    password: "password",
+    password_confirmation: "password"
+)
+
+booking_type1 = BookingType.create!(
+    color: "#28bdf8",
+    description: "15 min Test 123",
+    duration: 15,
+    location: "zoom",
+    name: "15min",
+    payment_required: false,
+    price: nil,
+    user: user
+)
+
+booking_type2 = BookingType.create!(
+    color: "#fbbf24",
+    description: "30 min Test 123",
+    duration: 30,
+    location: "zoom",
+    name: "30min",
+    payment_required: false,
+    price: nil,
+    user: user
+)
+
+booking_type3 = BookingType.create!(
+    color: "#34d399",
+    description: "50 min Test 123",
+    duration: 60,
+    location: "zoom",
+    name: "60min",
+    payment_required: true,
+    price: 125,
+    user: user
+)
+
